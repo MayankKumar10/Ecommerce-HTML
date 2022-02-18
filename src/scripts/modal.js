f938fb73aa2dcf9d63331b4df0e331e816559aa6
Mayank Kumar <53262974+MayankKumar10@users.noreply.github.com>
2022-02-18 23:14:15 +0530

    Product page, filter, and Wishlist (#5)
    * Adding index file to netify
    
    Adding index file to netify
    
    * Product Page, Filter, and Wishlist
    
    Adding Product Page, Filter, and Wishlist
    to Ecommerce Project
    
    * Refactoring code
    
    refactoring product page code

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -6,6 +6,24 @@ var modal =
 var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = ()=> {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = ()=> {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = (event)=> {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 var span =
 	document.getElementsByClassName(
 		"close"
